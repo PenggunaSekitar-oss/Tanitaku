170931ed0bb74a5b6e0bff3caf90a4c5175dc158
Pk Dplq Duvupg <muhaminarsyad969@gmail.com>
2026-07-28 18:21:02 +0800

    fix: pass luasManualM2 to calculateLuasLahan in totalLuasLahan KPI

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -349,7 +349,8 @@ export function DashboardView({ navigate }: { navigate: (v: string) => void }) {
         blok.jumlahBedengan,
         blok.panjangBedengan,
         blok.lebarBedengan,
-        blok.jarakAntarBedengan
+        blok.jarakAntarBedengan,
+        blok.luasManualM2,
       ),
     0
   );
